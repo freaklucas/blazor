d353d5f8f1aadeb86e36965e6d7d4af9e2f40326
freaklucas <lucas.oliveira.freak@gmail.com>
2023-05-08 17:28:06 -0300

    add rota

---
+++ BlazingPizza/Model/Drinks.cs
@@ -0,0 +1,8 @@
+namespace DrinksPizzaria {
+  public class Drinks {
+    public int Id { get; set; }
+    public decimal Value { get; set; }
+    public string Name { get; set; }
+    public decimal Weight { get; set; }
+  }
+}
