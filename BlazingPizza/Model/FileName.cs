a6dd0e4797c6b54a7c4830bb0814f1471e8b5aea
freaklucas <lucas.oliveira.freak@gmail.com>
2023-05-16 23:46:00 -0300

    add new class

---
+++ BlazingPizza/Model/FileName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BlazingPizza.Model
+{
+    public class FileName
+    {
+        public static void Main()
+        {
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue = Math.Max(firstValue, secondValue);
+            Console.WriteLine(largerValue);
+           
+        }
+
+       
+    }
+}
---
+++ Setup/Data/Example.cs
@@ -0,0 +1,66 @@
+using System;
+using Classes;
+using Classes2;
+using Classes3;
+namespace Classes
+{
+    public class People
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int Shoes {get; set;} = 2;
+        public bool Walk {get; set;}
+        public int Height { get; set; } = 160;
+
+        public string SumAgeAndHeight => $"{Age} + {Height}";
+    }
+}
+
+namespace Classes2
+{
+    public class Animal
+    {
+        public string? Name { get; set; }
+        public string? Action { get; set; }
+
+        public void Say()
+        {
+            Console.WriteLine($"Gatinho diz: {Action}");
+        }
+    }
+}
+
+
+namespace Classes3 {
+    class Alien : Classes.People {
+        new public string? Name {get;set;} 
+        public bool Antenna {get;set;}
+        public int VisitedPlanets {get;set;}
+        new public int Height {get;set;}
+
+        public void Fluttuent() {
+            Console.WriteLine($"Hello Humans, i Fluttuent, my name is {Name}");
+        }
+
+        public void Consol() {
+            Console.WriteLine("Oi!!!!!!!!!!!");
+        }
+    }
+}
+
+namespace ExampleComplete {
+    public class Elements {
+        public  static void Main() {
+            People pessoa = new People();
+            pessoa.Name = "João";
+            pessoa.Age = 20;
+            pessoa.Walk = true;
+            pessoa.Height = 180;
+            Console.WriteLine(pessoa.SumAgeAndHeight);
+        
+            Alien alien = new Alien();
+
+            alien.Consol();
+        }
+    }
+}
